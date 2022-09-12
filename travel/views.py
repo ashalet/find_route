7502933b75036f26714ee5649d8758ea53be7303
Kirill Borisenko <kirill.borisenko03@gmail.com>
2022-09-12 23:40:25 +0300

    добавил пару шаблонов

--- travel/views.py
+++ travel/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home(request):
     empty = "empty"
     return render(request, 'home.html', {'empty': empty})
+
+
+def about(request):
+    empty = "about"
+    return render(request, 'about.html', {'empty': empty})
